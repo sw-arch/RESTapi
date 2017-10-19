023c9676e458aedb5ced9cf90194159be5e0ac75
Jacob Mason <jacob@jacobmason.net>
2017-10-19 16:39:59 -0500

    Simplify Event and Ticket objects

--- app/data_objects/Event.py
+++ app/data_objects/Event.py
@@ -1,15 +1,20 @@
 from .Ticket import Ticket
 
+
 class Event:
-    def __init__(self, eventName, description, seats):
+    def __init__(self, eventName, description, seats, ticketPrice):
         self.eventName = eventName
         self.description = description
-        self.seats = seats
-        self.seatsLeft = seats
+        self.tickets = list(self.generate_tickets(seats))
+        self.ticketPrice = ticketPrice
+        self.seatsReserved = 0
 
     def reserve_ticket(self):
-        if self.seatsLeft > 0:
-            self.seatsLeft -= 1
-            return True
-        else:
-            return False
+        if self.seatsReserved < len(self.tickets):
+            self.seatsReserved += 1
+            return self.tickets[self.seatsReserved - 1]
+        return None
+
+    def generate_tickets(self, seats):
+        for ticketNumber in range(seats):
+            yield Ticket(self.ticketPrice, ticketNumber)
